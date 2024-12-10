4f54ce94ad59756992f863a5247a68fc6bf15c40
Gabriel Piazenski <gabrielpiazenski@gmail.com>
2024-12-09 22:58:56 -0300

    🚧 cleaned initial test from main springApplication run

--- backend/src/main/java/com/votacao/desafiovotacao/DesafiovotacaoApplication.java
+++ backend/src/main/java/com/votacao/desafiovotacao/DesafiovotacaoApplication.java
@@ -2,21 +2,11 @@ package com.votacao.desafiovotacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@RestController
+@SpringBootApplication
 public class DesafiovotacaoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DesafiovotacaoApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
