8a51f34a1fb20be67b228cd0627923645ef955e7
Gabriel Piazenski <gabrielpiazenski@gmail.com>
2024-12-09 22:57:00 -0300

    🚧 fixed the agenda repository

--- backend/src/main/java/com/votacao/desafiovotacao/infra/AgendaRepository.java
+++ backend/src/main/java/com/votacao/desafiovotacao/infra/AgendaRepository.java
@@ -2,6 +2,8 @@ package com.votacao.desafiovotacao.infra;
 
 import com.votacao.desafiovotacao.domain.entities.Agenda;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgendaRepository extends MongoRepository<Agenda, String> {
 }
